86359267d8c562a7a3bc69286a43b938e951d59e
Asus <Asus@DESKTOP-9JJ3D9B>
2021-02-17 20:57:01 +0300

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestTakePic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(674, 429);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(607, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(674, 429);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace TestTakePic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private static class Win32Native
+        {
+            public const int DESKTOPVERTRES = 0x75;
+            public const int DESKTOPHORZRES = 0x76;
+
+            [DllImport("gdi32.dll")]
+            public static extern int GetDeviceCaps(IntPtr hDC, int index);
+        }
+
+
+        Bitmap memoryImage;
+        private void CaptureScreen()
+        {
+
+            int width, height;
+            using (var g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                var hDC = g.GetHdc();
+                width = Win32Native.GetDeviceCaps(hDC, Win32Native.DESKTOPHORZRES);
+                height = Win32Native.GetDeviceCaps(hDC, Win32Native.DESKTOPVERTRES);
+                g.ReleaseHdc(hDC);
+            }
+            int y = this.Top;
+            this.Top = Screen.PrimaryScreen.Bounds.Height + 1000;
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = new Size(width , height );
+            memoryImage = new Bitmap(width , height , myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+            memoryGraphics.CopyFromScreen(Screen.PrimaryScreen.Bounds.Left, Screen.PrimaryScreen.Bounds.X, 0, 0, s);
+            this.Top = y;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CaptureScreen();
+            memoryImage.Save("Pic.jpg");
+            pictureBox1.Image = Image.FromFile("Pic.jpg");
+            button1.Location = new Point(Screen.PrimaryScreen.Bounds.Width - button1.Width, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.Bounds.Height) ;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            this.TopMost = true;
+
+        }
+        Point RectStartPoint;
+        Rectangle Rect = new Rectangle();
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            RectStartPoint = e.Location;
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            Rect.Location = new Point(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
+            Rect.Size = new Size(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(Pens.Blue, Rect);
+            
+        }
+
+        Bitmap memoryImage1;
+        private void PicRect()
+        {
+            int y = this.Top;
+            this.Top = Screen.PrimaryScreen.Bounds.Height + 1000;
+
+            Graphics myGraphics = this.CreateGraphics();
+            Size s = new Size(Rect.Width , Rect .Height );
+            memoryImage1 = new Bitmap(Rect .Width , Rect.Height , myGraphics);
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage1);
+            memoryGraphics.CopyFromScreen(Rect .X , Rect .Y, 0, 0, s);
+
+            this.Top = y;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+            PicRect();
+            memoryImage1.Save("Test1.jpg");
+        }
+
+
+    }
+}
